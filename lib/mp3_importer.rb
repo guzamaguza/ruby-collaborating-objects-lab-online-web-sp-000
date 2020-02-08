46d2c4965989fcdba4df4d880bce719c2f0620d9
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 03:40:03 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir[@path+"/*.mp3"].map do |file|
+      file.split("/").last
+    end
+  end
+
+  def import
+    Song.new_by_filename(files)
+    #files.each do |filename|
+    #  song = Song.new_by_filename(filename)
+    #end
+  end
+
+end
